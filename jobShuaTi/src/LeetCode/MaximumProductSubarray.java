e7ddd35a2046b60a64a93c0170bf48e8fdf97cae
liyongchun <li948241252@163.com>
2017-05-14 23:45:17 +0800

    Max num product

---
+++ jobShuaTi/src/LeetCode/MaximumProductSubarray.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+public class MaximumProductSubarray {
+	public int maxProduct(int[] nums) {
+        int res=nums[0];
+        int maxPre=nums[0];
+        int minPre=nums[0];
+        int maxhere,minhere;
+        
+        for (int i=1;i<nums.length;++i){
+            maxhere=Math.max(Math.max(maxPre*nums[i],minPre*nums[i]),nums[i]);
+            minhere=Math.min(Math.min(maxPre*nums[i],minPre*nums[i]),nums[i]);
+            res=Math.max(maxhere,res);
+            maxPre=maxhere;
+            minPre=minhere;
+        }
+        return res;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
