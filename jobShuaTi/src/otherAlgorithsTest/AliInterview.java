8f5808432cc8c0ae6b78767885638f6d55a192a3
suifeng <suifeng@suifeng_id>
2017-05-22 18:32:53 +0800

    阿里面试code

---
+++ jobShuaTi/src/otherAlgorithsTest/AliInterview.java
@@ -0,0 +1,33 @@
+package otherAlgorithsTest;
+
+import java.util.Stack;
+
+public class AliInterview {
+	public static boolean isTrue(String s){
+		if (s.length()==0) return false;
+		Stack<Character> stack=new Stack<>();
+		for (int i=0;i<s.length();++i){
+			if (s.charAt(i)=='('){
+				stack.push('(');
+			}else if (s.charAt(i)==')' && !stack.isEmpty()){
+				stack.pop();
+			}else if(s.charAt(i)==')' && stack.isEmpty()){
+				return false;
+			}
+			else{
+				continue;
+			}
+		}
+		if (stack.isEmpty()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(isTrue(")"));
+
+	}
+
+}
