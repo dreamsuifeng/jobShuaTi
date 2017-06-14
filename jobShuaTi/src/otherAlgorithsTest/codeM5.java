87bb58993640c8240c9749c53b1a2291fb602590
Yongchun Li <li948241252@163.com>
2017-06-15 01:49:48 +0800

    codeM比赛第五题测试O(n)算法滕鸥50%

--- jobShuaTi/src/otherAlgorithsTest/codeM5.java
+++ jobShuaTi/src/otherAlgorithsTest/codeM5.java
@@ -6,50 +6,54 @@ import java.util.Scanner;
 
 public class codeM5 {
 	public static int[] solve(int l, int r){
-		Map<Integer, int[]> map=new HashMap<>();
 		int[] res=new int[9];
-		
-		for (int i=l;i<=r;++i){
-			int[] tmp=new int[9];
-	            int sq= (int)Math.sqrt(i);
-	      
-	            for (int j=sq;j>=1;--j){
-	                if (i%j==0) {
-	                	if(map.containsKey(i)){
-	                		tmp=map.get(i);
-	                		break;
-	                	}else{
-	                		String t=String.valueOf(j);
-		                    tmp[Integer.parseInt(""+t.charAt(0))-1]++;
-		                    String ts=String.valueOf(i/j);
-		                    tmp[Integer.parseInt(""+ts.charAt(0))-1]++;
-	                	}
-	                    
-	                }
-	            }
-	            
-	           
-	            if (sq*sq==i){
-	              	tmp[Integer.parseInt(""+String.valueOf(sq).charAt(0))-1]--;
-	            }
-	            for (int k=0;k<9;k++){
-	            	res[k]+=tmp[k];
-	            }
-	            if (!map.containsKey(i)){
-	            	map.put(i,tmp);
-	            }
-	            
-	        }
+		res[0]=r-l+1;
+		System.out.println(1+","+res[0]);
+		for (int i=2;i<=r;++i){
+			int count=0;
+			if (i>=l){
+				count=(r-i)/i+1;
+			}else{
+				int diff=l%i==0 ? l:l+i-l%i;
+				if (diff>r) count=0;
+				else count=(r-diff)/i+1;
+			}
+			System.out.println(i+","+count);
+			int index=i/(int)Math.pow(10, (int)Math.log10(i));
+			res[index-1]+=count;
+		}
 		 return res;
+	}
+	public static int[] solve1(int l, int r){
+		int[] res=new int[9];
+		for (int i=l;i<=r;++i){
+			           
+           int sq= (int)Math.sqrt(i);
+            for (int j=1;j<=sq;++j){
+               if (i%j==0) {
+                    String t=String.valueOf(j);
+                    res[Integer.parseInt(""+t.charAt(0))-1]++;
+                    String ts=String.valueOf(i/j);
+                   res[Integer.parseInt(""+ts.charAt(0))-1]++;
+                }
+            }
+           
+            if (sq*sq==i){
+              	res[Integer.parseInt(""+String.valueOf(sq).charAt(0))-1]--;
+           }
+            
+        }
+		return res;
 	}
 	public static void main(String[] args){
         Scanner in=new Scanner(System.in);
         int l=in.nextInt();
         int r=in.nextInt();
         int[] res=solve(l, r);
+//        int[] res2=solve1(l, r);
        
-        for (Integer i:res){
-            System.out.println(i);
+        for (int i=0;i<9;++i){
+            System.out.print(res[i]+"\n");
         }
     }
 }
