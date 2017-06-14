51a5a3d509c339012cce4b45a248425d1a009628
Yongchun Li <li948241252@163.com>
2017-06-15 00:03:16 +0800

    create branch codeM4 by solve answer's circle

--- jobShuaTi/src/otherAlgorithsTest/codeM4.java
+++ jobShuaTi/src/otherAlgorithsTest/codeM4.java
@@ -9,29 +9,34 @@ import java.util.Set;
 import javax.sql.rowset.Joinable;
 
 public class codeM4 {
-	public static boolean solve(LinkedList<String> res, int[] a, int[] b, int n, int i, Set<Integer> exindex){
-		if (i==n-1) return true;
-		if (i>=n || i<0) return  false;
-		if (exindex.contains(i)) return false;
+	public static String solve(LinkedList<String> res, int[] a, int[] b, int n, int i, Set<Integer> exindex){
+		if (i==n-1) return "true";
+		if (i>=n || i<0) return  "false";
+		if (exindex.contains(i)) return "circle";
 		exindex.add(i);
-
-		if (a[i]!=0 ){
-			boolean t=solve(res,a, b, n, i+a[i], exindex);
-			if (t){
-				res.addFirst("a");
-				return true;
-			}
-			else{
-				if (b[i]!=0 && solve(res, a, b, n, i+b[i], exindex)){
-					res.addFirst("b");
-					return true;
-				}
+		String t=solve(res,a, b, n, i+a[i], exindex);
+		if (t.equals("true")){
+			res.addFirst("a");
+			return "true";
+		}else if (t.equals("Infinity!")){
+			return "Infinity!";
+		
+		}else{
+			
+			String tb=solve(res, a, b, n, i+b[i], exindex);
+			if (tb.equals("true") && t.equals("circle")){
+				return "Infinity!";
+			}else if(tb.equals("true") && t.equals("false")){
+				res.addFirst("b");
+				return "true";
+			}else if (tb.equals("false") || tb.equals("circle")){
+				return "false";
 			}
 		}
 		exindex.remove(i);
-		return false;
-		
+		return "false";
 	}
+
 	public static String join(LinkedList<String> s){
 		StringBuffer sb=new StringBuffer();
 		while(!s.isEmpty()){
@@ -52,11 +57,13 @@ public class codeM4 {
         	b[i]=in.nextInt();
         }
         LinkedList<String> opt=new LinkedList<>();
-        boolean flag=solve(opt,a, b,n, 0, new HashSet<Integer>());
-        if (flag){
+        String flag=solve(opt,a, b,n, 0, new HashSet<Integer>());
+        if (flag.equals("true")){
         	System.out.println(join(opt));	
-        }else{
+        }else if (flag.equals("false")){
         	System.out.println("No solution!");
+        }else{
+        	System.out.println("Infinity!");
         }
 //		}
 	}
