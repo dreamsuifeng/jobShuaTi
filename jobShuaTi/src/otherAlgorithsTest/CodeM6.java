33d3ee11bc50cf6662e6dac0e0a08472f2d75f23
liYongchun <li948241252@163.com>
2017-06-14 23:19:20 +0800

    modify codeM

---
+++ jobShuaTi/src/otherAlgorithsTest/CodeM6.java
@@ -0,0 +1,22 @@
+package otherAlgorithsTest;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class CodeM6 {
+	public void solve() {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+        int ngroup = in.nextInt();
+        int i=0;
+        while(i<ngroup){
+        	int n=in.nextInt();
+        	
+        }
+
+	}
+}
--- jobShuaTi/src/otherAlgorithsTest/codeM4.java
+++ jobShuaTi/src/otherAlgorithsTest/codeM4.java
@@ -1,40 +1,42 @@
 package otherAlgorithsTest;
 
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
+import javax.sql.rowset.Joinable;
+
 public class codeM4 {
-	public static String solve(int[] a, int[] b, int n, int i, String tmp, Set<Integer> exindex){
-		if (i==n-1) return tmp;
-		if (i>=n || i<0) return  "“No solution!”";
-		if (exindex.contains(i)) return "“Infinity!”";
-		String tmp1="“No solution!”";
+	public static boolean solve(LinkedList<String> res, int[] a, int[] b, int n, int i, Set<Integer> exindex){
+		if (i==n-1) return true;
+		if (i>=n || i<0) return  false;
+		if (exindex.contains(i)) return false;
 		exindex.add(i);
-		if (a[i]!=0){
-			
-			tmp1=solve(a, b, n, i+a[i], tmp+"a", exindex);
-//			exindex.remove(i);
-		}
-		if (!tmp1.equals("“No solution!”")){
-			return tmp1;
-		}else{
-			
-			String tmp2="“No solution!”";
-			if(b[i]!=0){
-//				exindex.add(i);
-				tmp2=solve(a, b, n, i+b[i], tmp+"b", exindex);
-				exindex.remove(i);
+		if (a[i]!=0 ){
+			boolean t=solve(res,a, b, n, i+a[i], exindex);
+			if (t){
+				res.addFirst("a");
+				return true;
 			}
-			if (!tmp2.equals("“No solution!”")){
-				return tmp2;
-			}else{
-				return "“No solution!”";
+			else{
+				if (b[i]!=0 && solve(res, a, b, n, i+b[i], exindex)){
+					res.addFirst("b");
+					return true;
+				}
 			}
 		}
-	
-	
+		exindex.remove(i);
+		return false;
+		
+	}
+	public static String join(LinkedList<String> s){
+		StringBuffer sb=new StringBuffer();
+		while(!s.isEmpty()){
+			sb.append(s.poll());
+		}
+		return sb.toString();
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -48,7 +50,13 @@ public class codeM4 {
         for(int i=0;i<n;i++){
         	b[i]=in.nextInt();
         }
-        System.out.println(solve(a, b,n, 0, "", new HashSet<Integer>()));
+        LinkedList<String> opt=new LinkedList<>();
+        boolean flag=solve(opt,a, b,n, 0, new HashSet<Integer>());
+        if (flag){
+        	System.out.println(join(opt));	
+        }else{
+        	System.out.println("No solution!");
+        }
 //		}
 	}
 
