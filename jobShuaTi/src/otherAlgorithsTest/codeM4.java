5adeecced986c5180eb84d85b2cdf4685d18fd98
Yongchun Li <li948241252@163.com>
2017-06-15 00:26:57 +0800

    codeM大赛第四题得出答案的字符串之后判断是否存在环

--- jobShuaTi/src/otherAlgorithsTest/codeM4.java
+++ jobShuaTi/src/otherAlgorithsTest/codeM4.java
@@ -6,10 +6,8 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
-import javax.sql.rowset.Joinable;
-
 public class codeM4 {
-	public static boolean solve(LinkedList<String> res, int[] a, int[] b, int n, int i, Set<Integer> exindex){
+	public  boolean solve(LinkedList<String> res, int[] a, int[] b, int n, int i, Set<Integer> exindex){
 		if (i==n-1) return true;
 		if (i>=n || i<0) return  false;
 		if (exindex.contains(i)) return false;
@@ -31,12 +29,31 @@ public class codeM4 {
 		return false;
 		
 	}
-	public static String join(LinkedList<String> s){
+	public  String join(LinkedList<String> s){
 		StringBuffer sb=new StringBuffer();
 		while(!s.isEmpty()){
 			sb.append(s.poll());
 		}
 		return sb.toString();
+	}
+	private boolean circle(int i, int[] a, int b[],int biaoji, int step){
+		if (i<0 || i>=a.length) return false;
+		if (step>0 && i==biaoji) return true;
+		return circle(i+a[i], a, b, biaoji,step+1) || circle(i+b[i], a, b, biaoji,step+1);
+	}
+	public boolean isCircle(char[] ans, int[] a, int[] b){
+		int start=0;
+		for (int i=0;i<ans.length;++i){
+			if (ans[i]=='a'){
+				if(circle(i, a, b, i, 0)){
+					return true;
+				}
+			}
+			if (ans[i]=='a') start+=a[start];
+			else start+=b[start];
+			
+		}
+		return false;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -51,9 +68,15 @@ public class codeM4 {
         	b[i]=in.nextInt();
         }
         LinkedList<String> opt=new LinkedList<>();
-        boolean flag=solve(opt,a, b,n, 0, new HashSet<Integer>());
+        codeM4 t=new codeM4();
+        boolean flag=t.solve(opt,a, b,n, 0, new HashSet<Integer>());
         if (flag){
-        	System.out.println(join(opt));	
+        	String answer=t.join(opt);
+        	if (t.isCircle(answer.toCharArray(), a, b)){
+        		System.out.print("Infinity!");
+        	}else{
+        		System.out.println(answer);
+        	}
         }else{
         	System.out.println("No solution!");
         }
