9db4781383f07436e847f43f9934a7121d82c492
liYongchun <li948241252@163.com>
2017-06-27 22:30:25 +0800

    conflict

--- jobShuaTi/src/NowcoderGAmeTest/outfromfarm.java
+++ jobShuaTi/src/NowcoderGAmeTest/outfromfarm.java
@@ -9,14 +9,17 @@ public class outfromfarm {
 //		int index=((k-1)*k/2)%n;
 //		int idk_1=((k-2)*(k-1)/2)%n;
 		for (int i=1;i<=n;++i){
-			for (int j=1;j<=Math.min(k, i);++j){
+			for (int j=1;j<=(int)Math.min(k, i);++j){
 				for (int l=0;l<n;++l){
 					if (i==1 && l==0){
 						dp[i][j][l]=1;
 					}
 					int t2=dp[i-1][j-1][l];
 					int index=(l+i-1)%n;
-					dp[i][j][index]=t2+dp[i-1][j][index];
+					dp[i][j][index]+=t2+dp[i-1][j][index];
+//					if (dp[i][j][index]!=0){
+//						System.out.println(i+","+j+","+index+" "+dp[i][j][index]);
+//					}
 				}
 			}
 		}
