ad6d6a1db3710d14a22c4c39ebf1c1bf6e532618
Yongchun Li <li948241252@163.com>
2017-07-07 11:33:03 +0800

    leetcode to 424 note completed

---
+++ jobShuaTi/src/LeetCode/LongestRepeatingCharacterReplacement424.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class LongestRepeatingCharacterReplacement424 {
+	public int characterReplacement(String s, int k) {
+		int len = s.length();
+        int[] count = new int[26];
+        int start = 0, maxCount = 0, maxLength = 0;
+        for (int end = 0; end < len; end++) {
+            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
+            while (end - start + 1 - maxCount > k) {
+                count[s.charAt(start) - 'A']--;
+                start++;
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+        return maxLength;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
