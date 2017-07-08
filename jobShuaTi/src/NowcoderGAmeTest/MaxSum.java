ccd013ddb44c2ef16c1f22b0fb531a859b2ac5c2
liyongchun <dreamsuifeng@gmail.com>
2017-07-08 09:47:16 +0800

    wangyi leihuo code test

---
+++ jobShuaTi/src/NowcoderGAmeTest/MaxSum.java
@@ -0,0 +1,17 @@
+package NowcoderGAmeTest;
+
+import java.util.Scanner;
+
+public class MaxSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		int N=in.nextInt();
+		int D=in.nextInt();
+		int[][] d=new int[N][N];
+		
+		
+	}
+
+}
