fc074ddff49e713fa9579bdd7bc0ec881eb0eff8
Yongchun Li <li948241252@163.com>
2017-07-10 21:44:38 +0800

    leetcode 456 completed

---
+++ jobShuaTi/src/LeetCode/Pattern132_456.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+import java.util.Stack;
+
+public class Pattern132_456 {
+	public boolean find132pattern(int[] nums) {
+        if(nums==null || nums.length<3) return false;
+        Stack<int[]> stack=new Stack<>();
+        for (int i:nums){
+        	if(stack.isEmpty() || i<stack.peek()[0]){
+        		int[] t=new int[2];
+        		t[0]=i;
+        		t[1]=i;
+        		stack.push(t);
+        	}else if (i>stack.peek()[0]){
+        		int[] t=stack.pop();
+        		if (t[1]>i) return true;
+        		else{
+        			t[1]=i;
+        			while (!stack.isEmpty() && i>=stack.peek()[1]) stack.pop();
+        			if (!stack.isEmpty() && stack.peek()[0]<i) return true;
+        			stack.push(t);
+        		}
+        	}
+        }
+        return false;
+    }
+}
